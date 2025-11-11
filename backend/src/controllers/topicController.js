15d4bf274d19b13c1d9ca29d6ff46cfa9957f116
Natalie <natalie.ann.kwak@gmail.com>
2025-11-11 13:59:30 -0500

    topicController added

---
+++ backend/src/controllers/topicController.js
@@ -0,0 +1,105 @@
+import mongoose from 'mongoose';
+import { Topic } from '../models/Topic.js';
+import { Assignment } from '../models/Assignment.js';
+
+// Get a topic by ID and assignment ID
+export const getTopicById = async (req, res) => {
+    try {
+        const { assignmentId, topicId } = req.params;
+
+        // Validate assignmentId
+        if (!mongoose.isValidObjectId(assignmentId)) {
+            return res.status(400).json({ error: "Invalid assignment ID" });
+        }
+
+        // Validate  topicId
+        if (!mongoose.isValidObjectId(topicId)) {
+            return res.status(400).json({ error: "Invalid topic ID" });
+        }
+
+        const topic = await Topic.findOne({ _id: topicId, assignmentId: assignmentId });
+
+        if (!topic) {
+            return res.status(404).json({ message: "Topic not found" });
+        }
+
+        res.status(200).json(topic);
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+// Create a topic document
+export const createTopic = async (req, res) => {
+    try {
+        const newTopic = new Topic({
+            assignmentId: req.params.assignmentId,
+            name: req.body.name,
+        });
+        await newTopic.save();
+
+        // Add topic reference to Assignment model
+        await Assignment.findByIdAndUpdate(
+            req.params.assignmentId,
+            { $push: { topicIds: newTopic._id, topicNames: newTopic.name } }
+        );
+
+        res.status(201).json(newTopic);
+    } catch (error) {
+        console.error("Error creating topic: ", error);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+// Update a topic document
+export const updateTopic = async (req, res) => {
+    try {
+        const { topicId } = req.params;
+
+        // Validate topicId
+        if (!mongoose.isValidObjectId(topicId)) {
+            return res.status(400).json({ error: "Invalid topic ID" });
+        }
+
+        const updatedTopic = await Topic.findByIdAndUpdate(
+            topicId,
+            req.body,
+            { new: true }
+        );
+
+        if (!updatedTopic) {
+            return res.status(404).json({ error: "Topic not found" });
+        }
+
+        res.status(200).json(updatedTopic);
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+// Delete a topic document
+export const deleteTopic = async (req, res) => {
+    try {
+        const { topicId } = req.params;
+
+        // Validate topicId
+        if (!mongoose.isValidObjectId(topicId)) {
+            return res.status(400).json({ error: "Invalid topic ID" });
+        }
+        const deletedTopic = await Topic.findByIdAndDelete(topicId);
+
+        if (!deletedTopic) {
+            return res.status(404).json({ error: "Topic not found" });
+        }
+
+        // Remove topic reference from Assignment model
+        await Assignment.findByIdAndUpdate(
+            deletedTopic.assignmentId,
+            { $pull: { topicIds: deletedTopic._id, topicNames: deletedTopic.name } }
+        );
+
+        res.status(200).json({ message: "Topic deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+};
--- backend/src/routes/writingGuideRoutes.js
+++ backend/src/routes/writingGuideRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { createAssignment, deleteAssignment, getAllAssignments, getAssignmentById, updateAssignment } from '../controllers/writingGuideController.js';
+import { getTopicById, createTopic, updateTopic, deleteTopic } from '../controllers/topicController.js';
 import { verifyUser } from '../utils/verification.js';
 
 const router = express.Router();
 
+// Basic routes for assignments
 router.get('/', verifyUser, getAllAssignments);
 
 router.get('/:assignmentId', verifyUser, getAssignmentById);
@@ -13,5 +15,14 @@ router.post('/', verifyUser, createAssignment);
 router.put('/:assignmentId', verifyUser, updateAssignment);
 
 router.delete('/:assignmentId', verifyUser, deleteAssignment);
+
+// Routes for a topic
+router.get('/:assignmentId/:topicId', verifyUser, getTopicById);
+
+router.post('/:assignmentId/topic', verifyUser, createTopic);
+
+router.put('/:assignmentId/:topicId', verifyUser, updateTopic);
+
+router.delete('/:assignmentId/:topicId', verifyUser, deleteTopic);
 
 export default router;
