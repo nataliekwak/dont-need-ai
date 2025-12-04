e068626d1824b42cd52b826a5b51c9f2ccb7a959
Natalie <natalie.ann.kwak@gmail.com>
2025-12-04 16:49:10 -0500

    vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,14 @@ import App from "./App.jsx";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-import { inject } from '@vercel/analytics';
-inject();
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HeroUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         <ToastProvider />
+        <Analytics />
         <BrowserRouter>
           <App />
         </BrowserRouter>
