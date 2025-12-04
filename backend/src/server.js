b923c11a72864ab89c93b0a81f78c042f8742687
Natalie <natalie.ann.kwak@gmail.com>
2025-12-03 20:05:55 -0500

    attempt to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,22 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+
 import authRoutes from './routes/authRoutes.js';
 import writingGuideRoutes from './routes/writingGuideRoutes.js';
 import { connectDB } from './db/connectDB.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true})); // Enable CORS
+if (process.env.NODE_ENV === 'production') {
+    app.use(cors({ origin: "http://localhost:5173", credentials: true})); // Enable CORS
+}
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
@@ -20,6 +25,14 @@ app.use(cookieParser()); // Middleware to parse cookies
 app.use('/api/auth', authRoutes);
 
 app.use('/api/writing-guide', writingGuideRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    });
+}
 
 // Start the backend server
 app.listen(PORT, () => {
